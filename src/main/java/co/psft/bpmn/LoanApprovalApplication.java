a25b0cd1d950ec605f183311fe88f7e04aa562ee
Angela Deady <angela.deady@perceptivesoftware.com>
2015-01-26 14:51:07 -0500

    initial commit

---
+++ src/main/java/co/psft/bpmn/LoanApprovalApplication.java
@@ -0,0 +1,9 @@
+package co.psft.bpmn;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class LoanApprovalApplication extends ServletProcessApplication {
+
+}
